e4e310d3f30e51d86c9354e9863d37d8af451999
Ernest Matola <mangochiman@gmail.com>
2018-11-23 15:25:20 +0200

    Outcomes

--- app/controllers/encounter_types_controller.rb
+++ app/controllers/encounter_types_controller.rb
@@ -2,6 +2,7 @@ class EncounterTypesController < GenericEncounterTypesController
 
   def index
   #raise current_user_roles.to_yaml
+    @patient = Patient.find(params[:patient_id])
     role_privileges = RolePrivilege.where(["role IN (?)", current_user_roles])
     privileges = role_privileges.each.map{ |role_privilege_pair| role_privilege_pair["privilege"].humanize }
 
